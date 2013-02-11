c68dcdca46151299bbc04c4bd56aa41536dda585
larmeh <larmeh@gmail.com>
2013-02-11 19:52:01 +0100

    Fail

--- Test/roman_int_test.rb
+++ Test/roman_int_test.rb
@@ -3,16 +3,16 @@ require 'roman_int'
 
 class TestRoman < Test::Unit::TestCase
 
-    attr_accessor :roman_int
+  attr_accessor :some_int
 
-    def setup
-        @roman_int = RomanInt.new
-    end
+  def setup
+    @some_int = RomanInt.new
+  end
 
-    def test_transform_all_and_back
-        (2**8).times { |i|
-            assert_equal(roman_int.int_to_roman(i), roman_int.roman_to_int(roman_int.int_to_roman(i)))
-        }
-    end
+  def test_transform_all_and_back
+    (2**8).times { |i|
+      assert_equal(i, some_int.int_to_roman(some_int.roman_to_int(i)))
+    }
+  end
 
 end
