e79f4c3dce5f4ecb0744354dd97cd73652d47a1e
bentzl <bentz@tzi.de>
2013-02-11 20:41:51 +0100

    test all tokens plus zero

--- Test/roman_int_test.rb
+++ Test/roman_int_test.rb
@@ -9,8 +9,21 @@ class TestRoman < Test::Unit::TestCase
     @converter = RomanInt.new
   end
 
-  def test_zero
+  def test_tokens
     assert_equal(converter.int_to_roman(0), "")
+    assert_equal(converter.int_to_roman(1), "I")
+    assert_equal(converter.int_to_roman(4), "IV")
+    assert_equal(converter.int_to_roman(5), "V")
+    assert_equal(converter.int_to_roman(9), "IX")
+    assert_equal(converter.int_to_roman(10), "X")
+    assert_equal(converter.int_to_roman(40), "XL")
+    assert_equal(converter.int_to_roman(50), "L")
+    assert_equal(converter.int_to_roman(90), "XC")
+    assert_equal(converter.int_to_roman(100), "C")
+    assert_equal(converter.int_to_roman(400), "CD")
+    assert_equal(converter.int_to_roman(500), "D")
+    assert_equal(converter.int_to_roman(900), "CM")
+    assert_equal(converter.int_to_roman(1000), "M")
   end
 
   def test_transform_all_and_back
