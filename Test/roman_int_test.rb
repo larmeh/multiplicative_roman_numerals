dcfd0708deb6de8f05b3606cce123752f093c9f7
larmeh <larmeh@gmail.com>
2013-02-12 10:22:04 +0100

    Smelly validity test

--- Test/roman_int_test.rb
+++ Test/roman_int_test.rb
@@ -9,12 +9,20 @@ class TestRoman < Test::Unit::TestCase
     @converter = RomanInt.new
   end
 
-  def test_transform_all_and_back
+  def test_valid_token
+    roman_nr = "XIIR"
+    # Soon.
+    #random_nr = Random.rand(2**32)
+    #roman_nr = converter.int_to_roman(random_nr)
+
+    # Smells, but works
+    valid_token = !roman_nr.match(/[^IVXLCDM\*\s]+/i)
+    assert(valid_token, "Non-roman number generated!")
+  end
 
   def test_transform_all_and_back
     (2**8).times { |i|
       assert_equal(i, converter.int_to_roman(converter.roman_to_int(i)))
     }
   end
-
 end
