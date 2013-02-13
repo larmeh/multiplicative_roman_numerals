b7d17efde37af08fe706c866d7e307580f0e6136
larmeh <larmeh@gmail.com>
2013-02-13 11:42:35 +0100

    That's one.

--- roman_int.rb
+++ roman_int.rb
@@ -1,9 +1,36 @@
 class RomanInt
-    def roman_to_int(input)
-        true
-    end
+  attr_reader :terminals
 
-    def int_to_roman(input)
-        false
-    end
+  def initialize
+    @terminals = {"M" => 1000, "CM" => 900, "D" => 500, "CD" => 400,
+      "C" => 100, "XC" => 90, "L" => 50,"XL" => 40, "X" => 10, "IX" => 9,
+      "V" => 5, "IV" => 4, "I" => 1, "*M " => 1000}
+  end
+
+  def roman_to_int(input)
+    result = 0
+  end
+
+  def int_to_roman(input)
+    result = ""
+
+    @terminals.each_pair{|key,val|
+      if (x = input/val) > 0 then
+        x.times do
+          result += key.to_s
+        end
+
+        #puts input.to_s + "/" + val.to_s + " ~= " + x.to_s
+        input -= x*val
+        #puts " R " + input.to_s
+      end
+    }
+
+    result
+  end
+
+  private
+  def factorize(number)
+
+  end
 end
