f5afe7908864e5512f83ef709065fbd6d5eb288e
larmeh <larmeh@gmail.com>
2013-02-14 02:30:18 +0100

    Finally working. All tests pass.

--- Test/roman_int_test.rb
+++ Test/roman_int_test.rb
@@ -19,16 +19,24 @@ class TestRoman < Test::Unit::TestCase
     assert(valid_token)
   end
 
-  def test_roman_to_int_borders
-
+  def test_communtative
+    assert_equal(249, converter.roman_to_int("XLCCIX"))
   end
 
-  #  def test_
+  def test_sanity_roman_int_roman
+    assert_equal("MMXIII",
+                 converter.int_to_roman(converter.roman_to_int("MMXIII")))
+  end
 
-  # Very useful test case: This caused me to discover that one element was
-  # missing from valid_numerals.
+  def test_sanity_int_roman_int
+    nr = 2013
+
+    assert_equal(nr,
+                 converter.roman_to_int(converter.int_to_roman(nr)))
+  end
+
   def test_tokens_int_roman
-    assert_equal(  "", converter.int_to_roman(0))
+    assert_equal("", converter.int_to_roman(0))
     assert_equal( "I", converter.int_to_roman(1))
     assert_equal("IV", converter.int_to_roman(4))
     assert_equal( "V", converter.int_to_roman(5))
@@ -45,14 +53,15 @@ class TestRoman < Test::Unit::TestCase
   end
 
   def test_tokens_roman_int
-    assert_equal( 1, converter.roman_to_int("I"))
-    assert_equal( 4, converter.roman_to_int("IV"))
-    assert_equal( 5, converter.roman_to_int("V"))
-    assert_equal( 9, converter.roman_to_int("IX"))
+    assert_equal(0, converter.roman_to_int(""))
+    assert_equal(1, converter.roman_to_int("I"))
+    assert_equal(4, converter.roman_to_int("IV"))
+    assert_equal(5, converter.roman_to_int("V"))
+    assert_equal(9, converter.roman_to_int("IX"))
     assert_equal(10, converter.roman_to_int("X"))
     assert_equal(40, converter.roman_to_int("XL"))
     assert_equal(50, converter.roman_to_int("L"))
-    assert_equal(60, converter.roman_to_int("XC"))
+    assert_equal(90, converter.roman_to_int("XC"))
     assert_equal(100, converter.roman_to_int("C"))
     assert_equal(400, converter.roman_to_int("CD"))
     assert_equal(500, converter.roman_to_int("D"))
@@ -60,15 +69,4 @@ class TestRoman < Test::Unit::TestCase
     assert_equal(1000, converter.roman_to_int("M"))
   end
 
-  def test_sanity_roman_int_roman
-    assert_equal("MMXIII",
-                 converter.int_to_roman(converter.roman_to_int("MMXIII")))
-  end
-
-  def test_sanity_int_roman_int
-    nr = 2013
-
-    assert_equal(nr,
-                 converter.roman_to_int(converter.int_to_roman(nr)))
-  end
 end
--- convert.rb
+++ convert.rb
@@ -1,29 +1,41 @@
 class Convert
-  attr_reader :terminals
+  attr_reader :terminals, :special
 
   def initialize
-    @terminals = {"M" => 1000, "CM" => 900, "D" => 500, "CD" => 400,
-      "C" => 100, "XC" => 90, "L" => 50,"XL" => 40, "X" => 10, "IX" => 9,
-      "V" => 5, "IV" => 4, "I" => 1, "*M " => 1000}
+    @special = [["CM", 900], ["CD" ,  400], ["XC" ,  90], ["XL" ,  40],
+      ["IX" ,  9], ["IV" ,  4]]
+    @terminals = [["M" ,  1000], ["D" ,  500], ["C" ,  100], ["L" ,  50],
+      ["X" ,  10], ["V" ,  5], ["I" ,  1]]
+    @multiplier = [["*M " ,  1000]]
+    @lang = @terminals.concat(@special).sort{ |x,y|
+      y[1] <=> x[1]
+    }
   end
 
   def roman_to_int(input)
     result = 0
     inputArr = input.split("")
 
+    lookahead = []
     inputArr.each_with_index do |val, i|
-      if inputArr.length > i
-        #lookahead = input[i+1]
-        lookahead = ""
-      else
-        lookahead = ""
+      if lookahead != []
+        lookahead = []
+        next
       end
 
-      concat = val + lookahead
-      if @terminals[concat] != nil
-        result += @terminals[concat]
+      if input[i+1] != nil
+        conc = val + input[i+1]
+        lookahead = @special.select{
+          |s| s.include? conc
+        }
+      end
+
+      if lookahead != []
+        result += lookahead[0][1]
       else
-        result += @terminals[val]
+        result += @terminals.select{
+          |s| s.include? val
+        }[0][1]
       end
     end
 
@@ -33,27 +45,18 @@ class Convert
   def int_to_roman(input)
     result = ""
 
-    @terminals.each_pair{|key,val|
-      if (x = input/val) > 0 then
+    @lang.each_index{ |i|
+      val = @lang[i][1]
+      key = @lang[i][0]
+      x = input/val
+      if x > 0
         x.times do
           result += key.to_s
         end
-
-        #puts input.to_s + "/" + val.to_s + " ~= " + x.to_s
         input -= x*val
-        #puts " R " + input.to_s
       end
     }
 
     result
   end
-
-  private
-  def factorize(number)
-
-  end
 end
-
-#instance = Convert.new
-#puts instance.int_to_roman(1337)
-#puts instance.roman_to_int("MCCCXXXVII")
